09244a467c94e8e76681e155d9dfa70d4574e204
XkoderFx <koder1554@gmail.com>
2021-01-26 17:21:51 +0200

    chnage colors in menu to white

--- components/layout/MainMenu.tsx
+++ components/layout/MainMenu.tsx
@@ -1,25 +1,25 @@
-import { AppBar, Button, Toolbar, Typography } from '@material-ui/core';
-import Link from 'next/link';
-import React from 'react';
+import { AppBar, Box, Button, Toolbar, Typography } from "@material-ui/core";
+import Link from "next/link";
+import React from "react";
 
 interface MainMenuProps {
-  brand: string;
-  items: string[];
+    brand: string;
+    items: string[];
 }
 
 const MainMenu: React.FC<MainMenuProps> = ({ brand, items }) => {
-  return (
-    <AppBar position="static">
-      <Toolbar>
-        <Typography variant="h3">{brand}</Typography>
-        {items.map((item, i) => (
-          <Link key={i} href="#">
-            <Button>{item}</Button>
-          </Link>
-        ))}
-      </Toolbar>
-    </AppBar>
-  );
+    return (
+        <AppBar position="static">
+            <Toolbar>
+                <Typography variant="h3">{brand}</Typography>
+                {items.map((item, i) => (
+                    <Button color="inherit" key={i + Date.now()}>
+                        {item}
+                    </Button>
+                ))}
+            </Toolbar>
+        </AppBar>
+    );
 };
 
 export default MainMenu;
